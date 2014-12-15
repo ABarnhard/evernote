3bed71de0fc94a74673e62457aaeef819d71e5a8
Adam Barnhard <admbarn@gmail.com>
2014-12-15 14:01:17 -0600

    changed id of test user

--- db/test/populate.sql
+++ db/test/populate.sql
@@ -1,1 +1,1 @@
-INSERT INTO users (id,username,password,avatar,token) VALUES (1,'bob','$2a$08$nCUsVXX7i7kMK7IuFIQMOOhZR4MGOlRSTd7aWL01jO7JsEiPGPUAW','https://s3.amazonaws.com/adam-evernote-test/token/avatar.png','token');
+INSERT INTO users (id,username,password,avatar,token) VALUES (1000,'bob','$2a$08$nCUsVXX7i7kMK7IuFIQMOOhZR4MGOlRSTd7aWL01jO7JsEiPGPUAW','https://s3.amazonaws.com/adam-evernote-test/token/avatar.png','token');
