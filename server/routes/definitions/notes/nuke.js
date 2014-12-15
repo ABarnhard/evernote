60db254b8b75253cd4e146e9a958d5952226c21f
Adam Barnhard <admbarn@gmail.com>
2014-12-15 10:40:52 -0600

    updated hapi and added tests

--- server/routes/definitions/notes/nuke.js
+++ server/routes/definitions/notes/nuke.js
@@ -11,6 +11,7 @@ module.exports = {
       noteId: Joi.number().required()
     }
   },
+  cors: {origin: ['http://localhost:8100'], credentials: true},
   handler: function(request, reply){
     Note.nuke(request.auth.credentials, request.params.noteId, function(err, noteId){
       reply({noteId:noteId}).code(err ? 400 : 200);
