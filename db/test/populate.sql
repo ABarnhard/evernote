1206a5b989158f40f578de7b425287f2af4b3620
Adam Barnhard <admbarn@gmail.com>
2014-12-15 14:12:00 -0600

    fixed insertion issue

--- db/test/populate.sql
+++ db/test/populate.sql
@@ -1,1 +1,7 @@
-INSERT INTO users (id,username,password,avatar,token) VALUES (1000,'bob','$2a$08$nCUsVXX7i7kMK7IuFIQMOOhZR4MGOlRSTd7aWL01jO7JsEiPGPUAW','https://s3.amazonaws.com/adam-evernote-test/token/avatar.png','token');
+INSERT INTO users (username,password,avatar,token) VALUES ('a1','b','c','d');
+INSERT INTO users (username,password,avatar,token) VALUES ('a2','b','c','d');
+INSERT INTO users (username,password,avatar,token) VALUES ('a3','b','c','d');
+
+DELETE FROM users;
+
+INSERT INTO users (id,username,password,avatar,token) VALUES (1,'bob','$2a$08$nCUsVXX7i7kMK7IuFIQMOOhZR4MGOlRSTd7aWL01jO7JsEiPGPUAW','https://s3.amazonaws.com/adam-evernote-test/token/avatar.png','token');
